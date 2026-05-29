170932d8ef4f128987072cfdd170076191a49eb3
Krishna Ravishankar <krishna.ravi732@gmail.com>
2026-05-28 20:50:45 -0400

    [dev][function-hooks] Ban static linkage hooks
    Static linkage for hooks don't make sense, as that would allow
    multiple compilation units to have conflicting hook definitions for
    the same symbol. Thus, it makes sense that hooks are extern linkage
    if only to prevent others from defining another hook for the same
    symbol. Moreover, the static specifier is a lie in q3asm and the
    symbols are globally visible anyway.
    
    This also changes the `#ifndef LINTER ... #else ... #endif` to
    `... #ifdef LINTER undef ... #endif` to allow linter checks to run
    on the actual not linter macro.

--- src/common/hook.h
+++ src/common/hook.h
@@ -16,32 +16,38 @@
 #define HOOKED(name) HOOK_LOCAL_EVAL_THEN_CONCAT_(name, HOOK_SUFFIX)
 #define ORIGINAL(name) HOOK_LOCAL_EVAL_THEN_CONCAT_(name, ORIG_SUFFIX)
 
-// rather than introducing 3 different macros (hook, orig, both), it feels
-// simpler to just do both here anyways...
 // NOLINTBEGIN(bugprone-macro-parentheses)
+
+// Yeah, this declares both the hook and the original, since they are often
+// tied together and it's simpler this way rather than introducing multiple
+// macros -_o_-
+// NOTE: The `extern` is to prevent anyone from mistakenly adding a static
+// specifier, as they don't quite make sense for hooks. It would allow multiple
+// compilation units to have conflicting hook definitions for the same symbol.
+// Thus, it makes sense that hooks are extern linkage if only to prevent others
+// from defining another hook for the same symbol. Moreover, the static
+// specifier is a lie in q3asm and the symbols are globally visible anyway.
 #define DECLARE_HOOK(ret_type, name, param_list) \
-    ret_type ORIGINAL(name) param_list;          \
-    ret_type HOOKED(name) param_list
-// NOLINTEND(bugprone-macro-parentheses)
+    extern ret_type ORIGINAL(name) param_list;   \
+    extern ret_type HOOKED(name) param_list
 
 // The original function being declared during hook definition is intended,
 // as most hooks will probably eventually call the original function.
-#ifndef LINTER
 #define DEFINE_HOOK(ret_type, name, param_list) \
     DECLARE_HOOK(ret_type, name, param_list)    \
     {
-#else
+#ifdef LINTER
+#undef DEFINE_HOOK
 // Shadow the original function with a function pointer inside the hook body to
 // generate a diagnostic if the original isn't used, as most hooks should
 // probably call the original at some point.
-// NOLINTBEGIN(bugprone-macro-parentheses)
 #define DEFINE_HOOK(ret_type, name, param_list) \
     DECLARE_HOOK(ret_type, name, param_list)    \
     {                                           \
         ret_type(*ORIGINAL(name)) param_list;
-// NOLINTEND(bugprone-macro-parentheses)
+#endif  // LINTER
 
-#endif
+// NOLINTEND(bugprone-macro-parentheses)
 
 #define END_HOOK }
 
