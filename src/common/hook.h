bc78cb8c9281f2929b2c4f956f27f1b63c08e062
Krishna Ravishankar <krishna.ravi732@gmail.com>
2026-05-29 11:45:59 -0400

    [dev][function-hooks] Prevent mismatches with any SDK declarations
    Also rename `DECLARE_HOOK` to be more accurate about what it does.

--- src/common/hook.h
+++ src/common/hook.h
@@ -16,37 +16,37 @@
 #define HOOKED(name) HOOK_LOCAL_EVAL_THEN_CONCAT_(name, HOOK_SUFFIX)
 #define ORIGINAL(name) HOOK_LOCAL_EVAL_THEN_CONCAT_(name, ORIG_SUFFIX)
 
-// NOLINTBEGIN(bugprone-macro-parentheses)
-
-// Yeah, this declares both the hook and the original, since they are often
-// tied together and it's simpler this way rather than introducing multiple
-// macros -_o_-
-// NOTE: The `extern` is to prevent anyone from mistakenly adding a static
+// NOTE:
+// 1. The `extern` is to prevent anyone from mistakenly adding a static
 // specifier, as they don't quite make sense for hooks. It would allow multiple
 // compilation units to have conflicting hook definitions for the same symbol.
 // Thus, it makes sense that hooks are extern linkage if only to prevent others
 // from defining another hook for the same symbol. Moreover, the static
 // specifier is a lie in q3asm and the symbols are globally visible anyway.
-#define DECLARE_HOOK(ret_type, name, param_list) \
-    extern ret_type ORIGINAL(name) param_list;   \
+// 2. The un-suffixed original symbol can't be used because the builder
+// hides it by design. It's just being declared here to help catch hook
+// signatures that don't match with sdk declarations if any.
+// NOLINTBEGIN(bugprone-macro-parentheses)
+#define DECLARE_ORIGINAL_AND_HOOK(ret_type, name, param_list) \
+    extern ret_type name param_list;                          \
+    extern ret_type ORIGINAL(name) param_list;                \
     extern ret_type HOOKED(name) param_list
 
 // The original function being declared during hook definition is intended,
 // as most hooks will probably eventually call the original function.
-#define DEFINE_HOOK(ret_type, name, param_list) \
-    DECLARE_HOOK(ret_type, name, param_list)    \
+#define DEFINE_HOOK(ret_type, name, param_list)           \
+    DECLARE_ORIGINAL_AND_HOOK(ret_type, name, param_list) \
     {
 #ifdef LINTER
 #undef DEFINE_HOOK
 // Shadow the original function with a function pointer inside the hook body to
 // generate a diagnostic if the original isn't used, as most hooks should
 // probably call the original at some point.
-#define DEFINE_HOOK(ret_type, name, param_list) \
-    DECLARE_HOOK(ret_type, name, param_list)    \
-    {                                           \
+#define DEFINE_HOOK(ret_type, name, param_list)           \
+    DECLARE_ORIGINAL_AND_HOOK(ret_type, name, param_list) \
+    {                                                     \
         ret_type(*ORIGINAL(name)) param_list;
 #endif  // LINTER
-
 // NOLINTEND(bugprone-macro-parentheses)
 
 #define END_HOOK }
