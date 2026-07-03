69ef9e8cf3bc3b085346f4628b6926f27dbfb8fa
Krishna Ravishankar <25255817+krsh732@users.noreply.github.com>
2026-07-03 12:55:49 -0400

    Allow personal teleporters to be picked up again
    DeFRaG explicitly prevents personal teleporters from being picked up in
    `DF_ItemPickupAllowed`. This is presumably due to fastcaps, as using
    the personal teleporter while holding the flag would cause the flag to
    drop. This would allow a timer reset off the dropped flag, potentially
    closer to one's own base. 
    
    However, this isn't great for regular run maps as mappers might
    actually want to have personal teleporters. 
    
    To address this, the personal teleporter is allowed to be picked up
    again and instead the flag is removed from the player without
    physically dropping it when the personal teleporter is used.
    
    Closes: #14
    PR: #40

---
+++ src/game/df_timer.c
@@ -0,0 +1,32 @@
+#include "qagame.h"
+
+DEFINE_HOOK(qboolean, DF_ItemPickupAllowed, (gentity_t* ent))
+    (void)ORIGINAL(DF_ItemPickupAllowed);  // intentional reimplementation
+
+    if (!ent->item) {
+        return qfalse;
+    }
+    if (ent->flags & FL_EXPLICIT_GIVE_CMD) {
+        return qtrue;
+    }
+
+    // DFE change: DeFRaG explicitly prevents personal teleporters from being
+    // picked up. Presumably because in fastcaps, using the personal teleporter
+    // while the flag is held, results in the flag dropping. Which in turn,
+    // would allow for unintended time resets off the dropped flag which may be
+    // closer to the player's own base. However, we want the personal teleporter
+    // to be usable once again, so we are going to let it be picked up. The
+    // issue with flags is taken care of in `ClientEvents`, see that for more
+    // info.
+    /*
+    if (ent->item->giType == IT_HOLDABLE && ent->item->giTag == HI_TELEPORTER) {
+        return qfalse;
+    }
+    */
+
+    // DFE change: The != 0 is just a nitpick thing I'm adding... We typed the
+    // return as qboolean in ReFRaG, but DeFRaG never actually brought the bit
+    // test result back to [qfalse, qtrue] (it doesn't matter cause all the
+    // existing usages just test with !DF_ItemPickupAllowed(...))
+    return (df.itemPickupAllowedBitmap & (1 << ent->item->giType)) != 0;
+END_HOOK
--- src/game/qagame.h
+++ src/game/qagame.h
@@ -21,8 +21,12 @@ FOR_EACH_CLIENT_COMMAND(DECLARE_COMMAND_)
 //
 // og df stuff that you don't know/care where to place
 //
+extern df_t df;
+
 void DF_ResetClientWaits(int clientNum);
 void DF_PlacePlayerTeleport(gentity_t* ent, vec3_t origin, vec3_t angles,
                             vec3_t velocity);
+gentity_t* SelectRandomFurthestSpawnPoint(vec3_t avoidPoint, vec3_t origin,
+                                          vec3_t angles, int arena);
 
 #endif  // QAGAME_HEADER_GUARD__
