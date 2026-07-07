f99a6ca9959fc6a6d61c653417e8d82aa7481f49
Krishna Ravishankar <krishna.ravi732@gmail.com>
2026-07-07 12:40:21 -0400

    [recall] Fix broken IT_HOLDABLE check

--- src/common/bg_savestate.c
+++ src/common/bg_savestate.c
@@ -18,13 +18,25 @@ qboolean DeserializeSaveState(const char* str, saveState_t* out)
     Base64_Decode(str, str_len, out);
     // TODO: bring it back to host endian (if host is different)
 
-    // These checks are to make sure things to don't go OOB and such, not so
-    // much as to prevent/disallow bogus/broken states.
-    return (out->holdable >= 0 && out->holdable < bg_numItems &&
-            bg_itemlist[out->holdable].giType == IT_HOLDABLE) &&
-           (out->weapon >= WP_NONE && out->weapon < WP_NUM_WEAPONS) &&
-           (out->weaponstate >= WEAPON_READY &&
-            out->weaponstate <= WEAPON_FIRING) &&
-           // TODO: would be nice to have a const for 0x20 in SDK
-           (out->num_checkpoints_hit >= 0 && out->num_checkpoints_hit < 0x20);
+    // The following checks are to make sure things to don't go OOB and such,
+    // not so much as to prevent/disallow bogus/broken states.
+    if (out->holdable < 0 || out->holdable >= bg_numItems) {
+        return qfalse;
+    }
+    if (out->holdable > 0 && bg_itemlist[out->holdable].giType != IT_HOLDABLE) {
+        return qfalse;
+    }
+
+    if (out->weapon < WP_NONE || out->weapon >= WP_NUM_WEAPONS) {
+        return qfalse;
+    }
+    if (out->weaponstate < WEAPON_READY || out->weaponstate > WEAPON_FIRING) {
+        return qfalse;
+    }
+
+    if (out->num_checkpoints_hit < 0 || out->num_checkpoints_hit >= 0x20) {
+        return qfalse;
+    }
+
+    return qtrue;
 }
